7bcc641e72c3c62aac4ca0c50a24675226a4e6a4
Evan Shortiss <evanshortiss@gmail.com>
2021-02-02 17:12:04 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const got = require('got')
+const { get } = require('env-var')
+const { CloudEvent, HTTP } = require('cloudevents')
+
+const fastify = require('fastify')({ logger: true })
+const BROKER_URL = get('BROKER_URL').default('http://broker-ingress.knative-eventing.svc.cluster.local/knative-eventing/default').asUrlString()
+
+fastify.get('/', async (req, reply) => {
+  const ce = new CloudEvent({
+    type: 'event-type',
+    data: {
+      hello: 'world'
+    },
+    source: 'fastify-http-server'
+  })
+  const message = HTTP.binary(ce)
+
+  fastify.log.info('sending cloud event %j', message)
+  const response = await got(BROKER_URL, {
+    method: 'POST',
+    body: message.body,
+    headers: message.headers
+  })
+
+  reply.send('ok')
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
